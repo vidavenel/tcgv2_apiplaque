b3ad9211cf19d73044d50777495a27a5889f8d7e
Vincent DAVENEL <vincent@aigle-developpement.com>
2019-06-11 11:32:29 +0200

    add laravel provider support

--- src/ApiplaqueServiceProvider.php
+++ src/ApiplaqueServiceProvider.php
@@ -8,5 +8,10 @@ use Illuminate\Support\ServiceProvider;
 
 class ApiplaqueServiceProvider extends ServiceProvider
 {
-
+    public function register()
+    {
+        $this->app->singleton(ApiplaqueService::class, function () {
+            return new ApiplaqueService(env('APIPLAQUE_TOKEN', 'demoxM'));
+        });
+    }
 }
